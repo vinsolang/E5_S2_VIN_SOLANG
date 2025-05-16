be6f863b55d5ce2426fd66a01c6c36daf92c6f3b
vinsolang <vinsolang9@gmail.com>
2025-05-16 21:46:43 +0700

    First commit

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ const icons = document.querySelector("#dropdownList");
 bar.onclick = ()=>{
     icons.classList.toggle("show");
 }
+
+
+function toggleDropdown() {
+  const dropdown = document.getElementById("dropdownList");
+  dropdown.classList.toggle("show");
+}
+
+// Close the dropdown if clicked outside
+window.onclick = function(event) {
+  if (!event.target.matches('.menu-button') && !event.target.closest('.menu-button')) {
+    const dropdowns = document.getElementsByClassName("dropdown-list");
+    for (let i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
